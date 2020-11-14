608b8d41c863ac01c845470bc94f4eb5c9b81f42
arefmalek <malek.aref1@gmail.com>
2020-11-13 23:41:48 -0800

    image for 2010 complete, trying electoral value plot now

--- aref/bar_plotting.py
+++ aref/bar_plotting.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 df = pd.read_csv("data_v1\ACSDP1Y2010.DP05_data_with_overlays_2020-11-06T214859.csv")
 df = df.drop(51)
-df = df.drop(0)
 
 # print(df["Geographic Area Name"], df["Estimate Margin of Error!!RACE!!One race!!Native Hawaiian and Other Pacific Islander!!Native Hawaiian"])
 # print(df.head())
 
+with open("data_v1\Electoral_votes.json") as f:
+    info = json.load(f)
+
+short_names = [val["name_short"] for val in info.values()]
+
 race = pd.DataFrame({
-    "State": df["NAME"],
+    "State": short_names,
     "White": df["DP05_0032PE"],
     "Black": df["DP05_0033PE"],
     "Asian": df["DP05_0039PE"],
@@ -19,14 +24,24 @@ race = pd.DataFrame({
     "Pacific Islander": df["DP05_0047PE"]
 })
 
-race["Native American"] = race["Native American"].replace("r'$", 0,regex=True)
-print(race)
+race["Native American"] = race["Native American"].replace("[N]", "0.0",regex=True)
 
+print(race)
 ind = race["State"]    # the x locations for the groups
 width = .8      # the width of the bars: can also be len(x) sequence
 percentages = [race["Black"], 
 race["Asian"], race["Latino"],
 race["Pacific Islander"], race["Native American"]]
+
+
+filler = plt.bar(
+    ind, np.full((51), 100, dtype=np.int32), 
+    width, color="lime"
+)
+
+white = plt.bar(
+    ind, race["White"], width,
+             bottom= 100 - race["White"], color="blue")
 
 
 black = plt.bar(
@@ -38,27 +53,26 @@ asian = plt.bar(
 )
 latino = plt.bar(
     ind, percentages[2], width,
-    bottom=sum(percentages[:2]), color="brown")
+    bottom=sum(percentages[:2]), color="brown"
+)
 islander = plt.bar(
     ind, percentages[3], width, 
-    bottom=sum(percentages[:3]), color="green")
-
+    bottom=sum(percentages[:3]), color="green"
+)
 
 native = plt.bar(
     ind, percentages[4], width,
-    bottom=sum(percentages[:4]), color="red"
+    bottom=sum(percentages[:4]), color="pink"
 )
 
 
-
-white = plt.bar(
-    ind, race["White"], width,
-             bottom= 100 - race["White"], color="blue")
-
-plt.ylabel('State')
+plt.ylabel('Percentage Makeup')
 plt.title('Percentage Breakdown of Race')
 
-plt.yticks(np.arange(0, 110, 10))
-plt.legend(('Black', "Asian", "Latino", "Native","Islander", 'White'))
+plt.legend(
+    [black, asian, latino, islander, native, white, filler], 
+    ["Black", "Asian", "Latino", "Islander", "Native", "White", "Other"] )
+plt.yticks(np.arange(0, 101, 10), range(0, 105, 10)) 
+plt.xticks(race["State"], rotation=90)
 
 plt.show()
--- aref/testing_file.py
+++ aref/testing_file.py
@@ -1,6 +1,18 @@
 import pandas as pd
+import numpy as np
+import json
+
+with open("data_v1\Electoral_votes.json") as f:
+    info = json.load(f)
 
 df = pd.read_csv("data_v1\ACSDP1Y2010.DP05_data_with_overlays_2020-11-06T214859.csv")
-df = df.drop(0)
+df = df.drop(51)
 
-print(df.head())
+short_names = [val["name_short"] for val in info.values()]
+
+p = pd.DataFrame({
+    "state": short_names,
+    "compare": df["NAME"]
+})
+
+print(p)
